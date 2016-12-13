be22c32c12a52450f76c751423ba9e664d1621e8
Jellezilla <jesper@tjorne.dk>
2016-12-14 00:02:55 +0100

    voronoi fix

--- CityGenerator/Assets/HeightmapParser.cs
+++ CityGenerator/Assets/HeightmapParser.cs
@@ -10,24 +10,21 @@ public class HeightmapParser : MonoBehaviour {
 
     public Texture2D heightmap;
 
+    public int tier1Chance;
+    public float tier1Threshold;
+    public int tier2Chance;
+    public float tier2Threshold;
+    public int tier3Chance;
+    public float tier3Threshold;
+
+
     private float height;
     private float width;
 
     [SerializeField]
-    private int amountOfDistricts;
+    public int amountOfDistricts;
 
-    private int
-        m_pointCount;
-
-    private List<Vector2> m_points;
-    private float m_mapWidth = 100;
-    private float m_mapHeight = 50;
-    private List<LineSegment> m_edges = null;
-    private List<LineSegment> m_spanningTree;
-    private List<LineSegment> m_delaunayTriangulation;
-
-    //  Color[,] pixelVals;
-
+    
     Dictionary<Vector2, float> densityValues = new Dictionary<Vector2, float>();
 
     List<Vector2> districtCenterPoints = new List<Vector2>();
@@ -36,76 +33,20 @@ public class HeightmapParser : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        m_pointCount = amountOfDistricts;
         height = heightmap.height;
         width = heightmap.width;
-
-        //pixelVals = new Color[(int)width, (int)height];
+        
         for(int x = 0; x < width; x++)
         {
             for(int y = 0; y < height; y++)
             {
-        //        pixelVals[x, y] = heightmap.GetPixel(x, y);
                 densityValues.Add(new Vector2(x, y), heightmap.GetPixel(x, y).grayscale);
 
             }
         }
-        /*
         
-        
-        Debug.Log("length: " + pixelVals.Length);
-        Debug.Log("width: " + width);
-        Debug.Log("height: " + height);
-        
-        maxVal = 0.0f;
-        
-        foreach (Color c in pixelVals)
-        {
-            if(c.grayscale > 0.85f)
-            {
-                Debug.Log("Color: " + c.grayscale.ToString());
-
-            }
-            if (c.grayscale > maxVal)
-            {
-                maxVal = c.grayscale;
-                
-            }
-        }
-        Debug.Log("maxval: " + maxVal);
-        */
-
-
     }
-
-    void Update()
-    {
-        
-        if(Input.anyKeyDown)
-        {
-
-            Demo();
-          //districtCenterPoints = GetDistrictCenterPoints();
-        }
-    }
-
-
-    void Demo()
-    {
-
-        List<uint> colors = new List<uint>();
-        m_points = new List<Vector2>();
-
-        m_points = GetDistrictCenterPoints();   
-        
-        Delaunay.Voronoi v = new Delaunay.Voronoi(m_points, colors, new Rect(0, 0, m_mapWidth, m_mapHeight));
-        m_edges = v.VoronoiDiagram();
-        /*
-        m_spanningTree = v.SpanningTree(KruskalType.MINIMUM);
-        m_delaunayTriangulation = v.DelaunayTriangulation();
-        */
-    }
-
+    
 
     public List<Vector2> GetDistrictCenterPoints()
     {
@@ -117,117 +58,61 @@ public class HeightmapParser : MonoBehaviour {
           
         myList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
 
-        
+
 
         //Debug.Log("list length: " + myList.Count);
-        
+
         // what about offsets to ensure District centers are positioned at a certain spacing?
-        for(int i = 0; i <= amountOfDistricts; i++)
+        for(int i = 0; i < amountOfDistricts; i++)
         {
             //Vector2 tmp;
-
+//            Debug.Log("I: " + i);
             int rand = Random.Range(1, 100);
-            if (rand > 35) // tier 1
+            if (rand > tier1Chance) // tier 1
             {
-                tmpList.Add(GetTierCoordinate(0.85f));
+                tmpList.Add(GetTierCoordinate(tier1Threshold));
                 
-            } else if (rand < 35 && rand > 10) // tier 2
+            } else if (rand < tier1Chance && rand > tier2Chance) // tier 2
             {
-                tmpList.Add(GetTierCoordinate(0.75f));
+                tmpList.Add(GetTierCoordinate(tier2Threshold));
             } else // tier 3
             {
-                tmpList.Add(GetTierCoordinate(0.55f));
+                tmpList.Add(GetTierCoordinate(tier3Threshold));
             }
-
         }
         return tmpList;
     }
-    /*
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Debug.Log("sa:" + districtCenterPoints.Count);
-        for (int i = 0; i < districtCenterPoints.Count; i++)
-        {
-            Gizmos.DrawSphere(districtCenterPoints[i], GetValFromCoordinates(districtCenterPoints[i]));
-        }
 
-    }
-    */
     float GetValFromCoordinates(Vector2 pos)
     {
-        return densityValues[pos];
+
+        try
+        {
+           
+            return densityValues[pos];
+        }
+        catch (KeyNotFoundException)
+        {
+            Debug.Log("Density count: " + densityValues.Count + " - trying to reach pos: (" + pos.x + "," + pos.y + "). Value: " + densityValues[pos]);
+            return 0.0f;
+        }
     }
 
     Vector2 GetTierCoordinate(float threshold)
     {
 
-        Vector2 tmp = new Vector2(Random.Range(0, 255), Random.Range(0, 255));
+        Vector2 tmp = new Vector2(Random.Range(20, 235), Random.Range(20, 235));
 
-        //int x = Random.Range(0, 255);
-        //int y = Random.Range(0, 255);
+
         if (GetValFromCoordinates(tmp) > threshold)
         {
             return tmp;
         }
         else
         {
-//            Debug.Log("Coordinate value did not match threshold. Trying again...");
             return GetTierCoordinate(threshold);
+            //return Vector2.zero;
         }
-
-        //return tmp;
     }
-    
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        if (m_points != null)
-        {
-            for (int i = 0; i < m_points.Count; i++)
-            {
-                Gizmos.DrawSphere(m_points[i], 0.2f);
-            }
-        }
-
-        if (m_edges != null)
-        {
-            Gizmos.color = Color.white;
-            for (int i = 0; i < m_edges.Count; i++)
-            {
-                Vector2 left = (Vector2)m_edges[i].p0;
-                Vector2 right = (Vector2)m_edges[i].p1;
-                Gizmos.DrawLine((Vector3)left, (Vector3)right);
-            }
-        }
-
-        Gizmos.color = Color.magenta;
-        if (m_delaunayTriangulation != null)
-        {
-            for (int i = 0; i < m_delaunayTriangulation.Count; i++)
-            {
-                Vector2 left = (Vector2)m_delaunayTriangulation[i].p0;
-                Vector2 right = (Vector2)m_delaunayTriangulation[i].p1;
-                Gizmos.DrawLine((Vector3)left, (Vector3)right);
-            }
-        }
-
-        if (m_spanningTree != null)
-        {
-            Gizmos.color = Color.green;
-            for (int i = 0; i < m_spanningTree.Count; i++)
-            {
-                LineSegment seg = m_spanningTree[i];
-                Vector2 left = (Vector2)seg.p0;
-                Vector2 right = (Vector2)seg.p1;
-                Gizmos.DrawLine((Vector3)left, (Vector3)right);
-            }
-        }
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(new Vector2(0, 0), new Vector2(0, m_mapHeight));
-        Gizmos.DrawLine(new Vector2(0, 0), new Vector2(m_mapWidth, 0));
-        Gizmos.DrawLine(new Vector2(m_mapWidth, 0), new Vector2(m_mapWidth, m_mapHeight));
-        Gizmos.DrawLine(new Vector2(0, m_mapHeight), new Vector2(m_mapWidth, m_mapHeight));
-    }
+        
 }

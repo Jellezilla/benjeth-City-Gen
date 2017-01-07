e15d0e46cb5211ffc50ee2f3f66c342b906d598a
Jellezilla <jesper@tjorne.dk>
2017-01-07 12:22:44 +0100

    BSP fix attempt (not working)

---
+++ CityGenerator/Assets/BSP.cs
@@ -0,0 +1,121 @@
+﻿/*
+    Author: Jesper Tjørnelund (jeth@itu.dk)
+    The BSP class is the class that initializes the Binary Search Partitioning method and creates Leafs. It takes in x, y, width and depth as parameters. 
+*/
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BSP : MonoBehaviour
+{
+
+    uint MAX_LEAF_SIZE = 20;
+
+    //public List<Leaf> leafs = new List<Leaf>();
+    public List<GameObject> leafs = new List<GameObject>();
+
+    List<GameObject> tmpList = new List<GameObject>();
+
+    int h;
+    int w;
+
+    Leaf root;
+
+    // Use this for initialization
+    void Start()
+    {
+        h = 100;
+        w = 100;
+        //GameObject root = new GameObject();
+        //root = new Leaf(0, 0, h, w);
+        GameObject root = new GameObject();
+        //Leaf root = new Leaf();
+        root.AddComponent<Leaf>();
+        root.GetComponent<Leaf>().Init(0, 0, h, w);
+        // root.AddComponent<Leaf>();
+        leafs.Add(root);
+
+        bool did_split = true;
+
+        DidSplit(did_split);
+        /*
+        while (did_split)
+        {
+            did_split = false;
+            foreach (GameObject go in leafs)
+            {
+                Leaf l = go.GetComponent<Leaf>();
+                if (l.leftChild == null && l.rightChild == null)
+                {
+                    if (l.width > MAX_LEAF_SIZE || l.height > MAX_LEAF_SIZE)
+                    {
+                        if (l.Split())
+                        {
+                            leafs.Add(l.leftChild);
+                            leafs.Add(l.rightChild);
+
+                            did_split = true;
+                        }
+                    }
+                }
+            }
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DidSplit(bool did_split)
+    {
+
+        //List<Leaf> tmpList = new List<Leaf>();
+        //List<GameObject> tmpList = new List<GameObject>();
+
+        did_split = false; 
+        foreach (GameObject go in leafs)
+        {
+            Leaf l = go.GetComponent<Leaf>();
+            if(l.leftChild == null && l.rightChild == null)
+            {
+                if (l.width > MAX_LEAF_SIZE || l.height > MAX_LEAF_SIZE)
+                {
+                    if (l.Split())
+                    {
+//                        leafs.Add(l.leftChild);
+//                        leafs.Add(l.rightChild);
+
+                        tmpList.Add(l.leftChild);
+                        tmpList.Add(l.rightChild);
+
+
+                        did_split = true;
+                        Debug.Log("tykke");
+                        DidSplit(did_split);
+                    }
+                } else {
+                    Debug.Log("Space too small, stopping...");
+                    return;
+                }
+            }
+        }
+        //UpdateList();
+    }
+
+    void UpdateList()
+    {
+        leafs.Add(tmpList[0]);
+        leafs.Add(tmpList[1]);
+        tmpList.Clear();
+        /*
+        foreach(GameObject go in tmpList)
+        {
+            
+            leafs.Add(go);
+            tmpList.Remove(go);
+        }*/
+    }
+}
---
+++ CityGenerator/Assets/Leaf.cs
@@ -0,0 +1,108 @@
+﻿/**
+    Author: Jesper Tjørnelund (jeth@itu.dk)
+    The Leaf class is used for the Binary Search Partitioning (BSP) and serves as a data type for each leaf node the BSP tree is divided into. 
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class Leaf : MonoBehaviour
+{
+
+
+    public int MIN_LEAF_SIZE = 6;
+
+    public int x, y, height, width;
+
+    //public Leaf leftChild;
+    //public Leaf rightChild;
+    public GameObject leftChild;
+    public GameObject rightChild;
+
+    public void Init(int _x, int _y, int _height, int _width)
+    {
+        this.x = _x;
+        this.y = _y;
+        this.height = _height;
+        this.width = _width;
+    }
+
+    /*    public Leaf(int _x, int _y, int _height, int _width)
+        {
+
+
+            this.x = _x;
+            this.y = _y;
+            this.height = _height;
+            this.width = _width;
+        }*/
+
+    public bool Split()
+    {
+        if (leftChild || rightChild)
+        {
+            Debug.Log("Already split. Aborting...!");
+            return false; // We're already split! ABORT!
+        }
+
+        bool splitH;
+        if (Random.Range(0.0f, 1.0f) > 0.5f) // 50-50 for split direction unless height is considerably larger than width or vice verca (see if statement below).
+        {
+            splitH = false;
+        }
+        else
+        {
+            splitH = true;
+        }
+
+        if (width > height && width / height >= 1.25)
+        {
+            splitH = false;
+        }
+        else if (height > width && height / width >= 1.25f)
+        {
+            splitH = true;
+        }
+
+        int max = (splitH ? height : width) - MIN_LEAF_SIZE;
+        if (max <= MIN_LEAF_SIZE)
+        {
+            Debug.Log("Area too small. Aborting split");
+            return false; // area too small
+        }
+
+        int split = Random.Range(MIN_LEAF_SIZE, max);
+
+        if (splitH)
+        {
+            //leftChild = new Leaf(x, y, split, width);
+            //rightChild = new Leaf(x, y + split, height - split, width);
+
+            leftChild = new GameObject();
+            rightChild = new GameObject();
+            leftChild.AddComponent<Leaf>();
+            rightChild.AddComponent<Leaf>();
+            leftChild.GetComponent<Leaf>().Init(x, y, split, width);
+            rightChild.GetComponent<Leaf>().Init(x, y + split, height - split, width);
+
+        }
+        else
+        {
+            //leftChild = new Leaf(x, y, height, split);
+            //rightChild = new Leaf(x + split, y, height, width - split);
+
+            leftChild = new GameObject();
+            rightChild = new GameObject();
+            leftChild.AddComponent<Leaf>();
+            rightChild.AddComponent<Leaf>();
+            leftChild.GetComponent<Leaf>().Init(x, y, height, split);
+            rightChild.GetComponent<Leaf>().Init(x + split, y, height, width - split);
+        }
+
+        Debug.Log("Split successful!");
+        return true;
+
+    }
+
+
+}
